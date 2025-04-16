5027ddc201db8eabe50ad6edc95a626a0cd6c335
Bryan Lai <bryanlais@gmail.com>
2025-04-16 18:04:57 +0800

    init: repo building darwin apps from source

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# build script for mac apps
+# in this script we assume gnu cli utils
+
+set -euo pipefail
+set -x
+
+cd "$(dirname "$0")"
+CODESIGNING=DEVELOPMENT_TEAM=AWMJ8H4G7B
+
+(
+  cd ./Ice
+  xcodebuild -scheme Ice -configuration Release \
+    -derivedDataPath ./DerivedData \
+    "$CODESIGNING"
+  cp -af --reflink=auto ./DerivedData/Build/Products/Release/Ice.app ../archive
+)
+
+pwd
