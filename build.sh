a095069609a9c94020adfd892409decbf7dbb6ab
Bryan Lai <bryanlais@gmail.com>
2025-04-19 00:58:11 +0800

    feat: try building & signing

--- build.sh
+++ build.sh
@@ -2,18 +2,19 @@
 # build script for mac apps
 # in this script we assume gnu cli utils
 
+CODESIGNING=DEVELOPMENT_TEAM=AWMJ8H4G7B
+ARCHIVE_DIR=archive/Applications
+
 set -euo pipefail
 set -x
 
 cd "$(dirname "$0")"
-CODESIGNING=DEVELOPMENT_TEAM=AWMJ8H4G7B
+mkdir -p "$ARCHIVE_DIR"
 
 (
   cd ./Ice
   xcodebuild -scheme Ice -configuration Release \
     -derivedDataPath ./DerivedData \
     "$CODESIGNING"
-  cp -af --reflink=auto ./DerivedData/Build/Products/Release/Ice.app ../archive/Applications
+  /bin/cp -acf ./DerivedData/Build/Products/Release/Ice.app ../"$ARCHIVE_DIR"
 )
-
-pwd
